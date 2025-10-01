d0c501bc38d0106924e01977c1e89d2233605dc4
berkayerten <berkay.erten@haiilo.com>
2025-10-01 23:15:57 +0200

    Java Server is simplified without Spring

--- grpc-server/src/main/java/com/tech_thrive_catalyst/grpc_server/GrpcServerApplication.java
+++ grpc-server/src/main/java/com/tech_thrive_catalyst/grpc_server/GrpcServerApplication.java
@@ -1,13 +1,20 @@
 package com.tech_thrive_catalyst.grpc_server;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionServiceV1;
+import java.io.IOException;
 
-@SpringBootApplication
 public class GrpcServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GrpcServerApplication.class, args);
+	public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(8174)
+                .addService(new UserServiceImpl(new UserRepository()))
+                .addService(ProtoReflectionServiceV1.newInstance())
+                .build();
+
+        server.start();
+        server.awaitTermination();
 	}
 
 }
